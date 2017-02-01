fbad56b487d7a76135ee0033470054b8049da096
Ben Barden <github@benbarden.com>
2017-02-01 23:26:30 +0000

    Album 10, track 1

--- public/assets/js/albums/ten.js
+++ public/assets/js/albums/ten.js
@@ -4,12 +4,7 @@ var playlistPath = "http://coda.city/downloads/ben-barden-music/ten/";
 var bbMusicTracklist = [];
 
 bbMusicTracklist.push({
-    title: "[ Coming soon! ]"
+    title: "A Morning in February",
+    mp3: playlistPath + encodeURI('01. Ben Barden - A Morning in February.mp3'),
+    duration: "4:02"
 });
-/*
-bbMusicTracklist.push({
-    title: "",
-    mp3: playlistPath + encodeURI('.mp3'),
-    duration: ""
-});
-*/
