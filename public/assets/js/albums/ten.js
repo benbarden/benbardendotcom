1b62c6a7814f55b70071c5aa9e208db2c1488e1e
Ben Barden <github@benbarden.com>
2017-02-02 21:33:30 +0000

    Album 10, track 2

--- public/assets/js/albums/ten.js
+++ public/assets/js/albums/ten.js
@@ -8,3 +8,8 @@ bbMusicTracklist.push({
     mp3: playlistPath + encodeURI('01. Ben Barden - A Morning in February.mp3'),
     duration: "4:02"
 });
+bbMusicTracklist.push({
+    title: "Lost in a Dream",
+    mp3: playlistPath + encodeURI('02. Ben Barden - Lost in a Dream.mp3'),
+    duration: "3:50"
+});
